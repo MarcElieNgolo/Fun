011dd841814c666a1a0890a197111cdd9a3fc8fc
MarcElieNgolo <eliediabate47@gmail.com>
2025-07-15 09:31:09 +0000

    Mise à jour du projet : fichiers modifiés, suppression et ajout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import  {BrowserRouter as Routeur , Routes, Route, useNavigate} from "react-router-dom"
 import Acceuil from "./../pages/Acceuil"
 import Rang from "../pages/Rang"
@@ -34,26 +33,3 @@ function Redirect(){
   })
   return null
 }
-=======
-import Generator from "./compenents/form";
-import Qrcode from "./compenents/qrcode";
-import { BrowserRouter as Routeur, Routes, Route, Navigate } from "react-router-dom";
-
-function App() {
-  return (
-    <Routeur>
-      <Routes>
-        {/* Redirection propre vers /acceuil */}
-        <Route path="/" element={<Navigate to="/acceuil" replace />} />
-
-        {/* Page d'accueil */}
-        <Route path="/acceuil" element={<Generator />} />
-        <Route path="/qrcode" element={<Qrcode />} /> 
-        <Route path="/qrcode/*" element={<Qrcode />} /> 
-      </Routes>
-    </Routeur>
-  );
-}
-
-export default App;
-
