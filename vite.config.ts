9e30b82912784df89207b386e5219d1579d411e1
MarcElieNgolo <eliediabate47@gmail.com>
2025-07-18 13:22:05 +0000

    redirection terrain

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // ⚠️ Très important pour les chemins relatifs !
+  base: '/', // ⚠️ Très important pour les chemins relatifs !
   plugins: [react()],
 });
